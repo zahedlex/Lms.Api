ba63f517b3bb74da37add823b706fcccf93fb6be
Zahed Davodi <zaheddavodibaloch@gmail.com>
2022-08-31 16:19:35 +0200

    Add project files.

---
+++ Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Lms.Data.Data;
+using Lms.Api;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lms.Api.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+
+        public static async Task<IApplicationBuilder> SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var db = serviceProvider.GetRequiredService<LmsApiContext>();
+
+                //db.Database.EnsureDeleted();
+                //db.Database.Migrate();
+
+                try
+                {
+                    await SeedData.InitAsync(db);
+                }
+                catch (Exception e)
+                {
+
+                    throw;
+                }
+            }
+
+            return app;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Lms.Data.Data;
+using Lms.Api.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<LmsApiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LmsApiContext") ?? throw new InvalidOperationException("Connection string 'LmsApiContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers(opt => opt.ReturnHttpNotAcceptable = true)
+                .AddNewtonsoftJson()
+                .AddXmlDataContractSerializerFormatters();
+
+var app = builder.Build();
+
+app.SeedDataAsync().GetAwaiter().GetResult();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
